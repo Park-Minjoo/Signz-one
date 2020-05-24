1b489728cdd86e6b3f9c112ac4134fd38f0adb9d
Sarah <sarahk1280@gmail.com>
2020-05-24 06:27:51 -0400

    fixed

--- Assets/Script/CameraObrbit.cs
+++ Assets/Script/CameraObrbit.cs
@@ -3,39 +3,59 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class CameraObrbit : MonoBehaviour
-{
-    public float MinDistance = -1000f;
-    public float MaxDistance = 130f;
-    float distance = 1000;
-    float distanceTarget;
-    Vector2 mouse;
-    Vector2 mouseOnDown;
-    Vector2 rotation;
-    Vector2 target = new Vector2(Mathf.PI * 3 / 2, Mathf.PI / 6);
-    Vector2 targetOnDown;
+{   
+    [SerializeField]
+    private float MinDistance = -1000f;
+    [SerializeField]
+    private float MaxDistance = 130f;
+    private float distance = 1000;
+    private float distanceTarget;
+    private Vector2 mouse;
+    private Vector2 mouseOnDown;
+    private Vector2 rotation;
+    private Vector2 target = new Vector2(Mathf.PI * 3 / 2, Mathf.PI / 6);
+    private Vector2 targetOnDown;
 
-    public float speed = 25.0f;
-    public Transform cameraTarget;
+    [SerializeField]
+    private float speed = 25.0f;
+    [SerializeField]
+    private Transform cameraTarget;
+
     private Camera thisCamera;
     private Vector3 worldDefalutForward;
 
 
-    // Use this for initialization
-    float time = 0;
-    float fades = 0.0f;
-    
+    private bool down = false;
+    private bool focusOn = false;
 
-    void Start () {
+
+
+    void Start()
+    {
+
         distanceTarget = transform.position.magnitude;
         thisCamera = GetComponent<Camera>();
         worldDefalutForward = transform.forward;
-        
+
     }
-    bool down = false;
     // Update is called once per frame
-    void Update()
+
+    void ZoomInput()
     {
-        if (Input.touchCount > 0)
+        float scroll = Input.GetAxis("Mouse ScrollWheel") * speed;
+    
+        if (thisCamera.fieldOfView <= 20.0f && scroll < 0) return; //최대 줌인
+        if (thisCamera.fieldOfView >= 70.0f && scroll > 0) return;//최대 줌 아웃
+
+        //줌인 아웃 하기
+        thisCamera.fieldOfView += scroll;
+    } // 줌인 줌아웃 input
+
+    void DragInput()
+    {
+#if UNITY_ANDROID || UNITY_IPHONE
+
+        if (Input.touchCount > 0) // 모바일입력
         {
             if(Input.touches[0].phase== TouchPhase.Began)
             {
@@ -52,23 +72,28 @@ public class CameraObrbit : MonoBehaviour
                 down = false;
             }
         }
-        else
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                down = true;
-                mouseOnDown.x = Input.mousePosition.x;
-                mouseOnDown.y = -Input.mousePosition.y;
+    
 
-                targetOnDown.x = target.x;
-                targetOnDown.y = target.y;
-            }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                down = false;
-            }
+#else
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            down = true;
+            mouseOnDown.x = Input.mousePosition.x;
+            mouseOnDown.y = -Input.mousePosition.y;
+
+            targetOnDown.x = target.x;
+            targetOnDown.y = target.y;
         }
-        if(down)
+        else if (Input.GetMouseButtonUp(0))
+        {
+            down = false;
+        }
+
+
+#endif
+
+        if (down) // 둘중하나 input 을 받는다면 
         {
             if (Input.touchCount > 0)
             {
@@ -82,32 +107,15 @@ public class CameraObrbit : MonoBehaviour
             }
             float zoomDamp = distance / 1;
 
-            target.x = targetOnDown.x + (mouse.x - mouseOnDown.x) * 0.005f* zoomDamp;
-            target.y = targetOnDown.y + (mouse.y - mouseOnDown.y) * 0.005f* zoomDamp;
-            
+            target.x = targetOnDown.x + (mouse.x - mouseOnDown.x) * 0.005f * zoomDamp;
+            target.y = targetOnDown.y + (mouse.y - mouseOnDown.y) * 0.005f * zoomDamp;
+
             target.y = Mathf.Clamp(target.y, -Mathf.PI / 2 + 0.01f, Mathf.PI / 2 - 0.01f);
         }
-        float scroll = Input.GetAxis("Mouse ScrollWheel") * speed;
 
-        //최대 줌인
-        if (thisCamera.fieldOfView <= 20.0f && scroll < 0)
-        {
-            thisCamera.fieldOfView = 20.0f;
-        }
 
-        //최대 줌 아웃
-        else if (thisCamera.fieldOfView >= 70.0f && scroll > 0)
-        {
-            thisCamera.fieldOfView = 70.0f;  
-		}
-        //줌인 아웃 하기
-        else
-        {
-            thisCamera.fieldOfView += scroll;
-		}
-        
         distanceTarget -= Input.GetAxis("Mouse ScrollWheel");
-         distanceTarget = Mathf.Clamp(distanceTarget, MinDistance, MaxDistance);
+        distanceTarget = Mathf.Clamp(distanceTarget, MinDistance, MaxDistance);
 
         rotation.x += (target.x - rotation.x) * 0.1f;
         rotation.y += (target.y - rotation.y) * 0.1f;
@@ -119,29 +127,56 @@ public class CameraObrbit : MonoBehaviour
         transform.position = position;
         transform.LookAt(Vector3.zero);
 
+    }
 
-        int layerMask = 1 << LayerMask.NameToLayer("Constellation");
-        SpriteRenderer render;
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    void FocusCheck()
+    {
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        if (!focusOn)
         {
-            Debug.Log("hit");
-            time += Time.deltaTime;
-            render = hit.transform.GetComponent<SpriteRenderer>();
-            if (fades < 1.0f && time >= 0.1f)
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            int layerMask = 1 << LayerMask.NameToLayer("Constellation");
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
             {
-                fades += 0.04f;
-                time = 0;
-                render.color = new Color(1, 1, 1, fades);
+
+                Debug.Log("FocusOn");
+                StartCoroutine(FocusConstellation(hit));
+                focusOn = true;
             }
+           
+
+        } 
+
+    }
+
+    IEnumerator FocusConstellation(RaycastHit focustarget)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        int layerMask = 1 << LayerMask.NameToLayer("Constellation");
+        Constellation temp = focustarget.collider.GetComponent<Constellation>();
+        temp.FadeStart();
+
+
+        while (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        { 
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);       
+            yield return null;
+                      
         }
-        else
-        {
-            time = 0;
-            fades = 0;
-        }
-        
+
+        Debug.Log("FocusOff");
+        temp.FadeReset();
+        focusOn = false;
+
+    }
+
+    void Update()
+    {
+        DragInput();
+        ZoomInput();
+        FocusCheck();
     }
 }
